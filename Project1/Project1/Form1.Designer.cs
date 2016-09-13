a8c2dbd1c3836d233cd53d6f225d349ec67eed52
win10vm <win10vm@DESKTOP-5T8U1IT>
2016-09-13 11:39:54 -0700

    proj 1 code done

---
+++ Project1/Project1/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace Project1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.input = new System.Windows.Forms.TextBox();
+            this.k_hole = new System.Windows.Forms.TextBox();
+            this.output = new System.Windows.Forms.TextBox();
+            this.button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // input
+            // 
+            this.input.Location = new System.Drawing.Point(26, 23);
+            this.input.Name = "input";
+            this.input.Size = new System.Drawing.Size(100, 20);
+            this.input.TabIndex = 0;
+            this.input.Text = "To check";
+            // 
+            // k_hole
+            // 
+            this.k_hole.Location = new System.Drawing.Point(26, 108);
+            this.k_hole.Name = "k_hole";
+            this.k_hole.Size = new System.Drawing.Size(100, 20);
+            this.k_hole.TabIndex = 1;
+            this.k_hole.Text = "Value of k";
+            // 
+            // output
+            // 
+            this.output.Location = new System.Drawing.Point(150, 52);
+            this.output.Multiline = true;
+            this.output.Name = "output";
+            this.output.Size = new System.Drawing.Size(100, 76);
+            this.output.TabIndex = 2;
+            this.output.Text = "Result";
+            // 
+            // button
+            // 
+            this.button.Location = new System.Drawing.Point(150, 23);
+            this.button.Name = "button";
+            this.button.Size = new System.Drawing.Size(100, 23);
+            this.button.TabIndex = 3;
+            this.button.Text = "Solve";
+            this.button.UseVisualStyleBackColor = true;
+            this.button.Click += new System.EventHandler(this.button_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 146);
+            this.Controls.Add(this.button);
+            this.Controls.Add(this.output);
+            this.Controls.Add(this.k_hole);
+            this.Controls.Add(this.input);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox input;
+        private System.Windows.Forms.TextBox k_hole;
+        private System.Windows.Forms.TextBox output;
+        private System.Windows.Forms.Button button;
+    }
+}
+
---
+++ Project1/Project1/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+
+namespace Project1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            long k = 5; 
+            if (! k_hole.Text.Contains("k"))
+            {
+                k = Convert.ToInt64(k_hole.Text);
+            }
+            if (prime_test(Convert.ToInt64(input.Text), k))
+            {
+                output.Text = input.Text + " is prime with k = " + k.ToString();
+            }
+            else
+            {
+                output.Text = input.Text + " is NOT prime with k = " + k.ToString();
+            }
+        }
+
+        private bool prime_test(long N, long k)
+        {
+            ISet < long > a = new HashSet<long>();
+            Random rand = new Random();
+
+            for (int i = 0; i < k; i++)
+            {
+                long t = rand.Next((int) N);
+                if (a.Contains(t))
+                {
+                    i--;
+                }
+                else
+                {
+                    a.Add(t);
+                    if(modexp(t, N - 1, N) != 1)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        long modexp(long x, long y, long N)
+        {
+            if (y == 0)
+            {
+                return 1;
+            }
+            if (y % 2 == 0)
+            {
+                long z = modexp(x, y / 2, N);
+                return  (z * z) % N;
+            }
+            else
+            {
+                long z = modexp(x, (y - 1) / 2, N);
+                return (x * (z * z)) % N;
+            }
+        }
+    }
+}
